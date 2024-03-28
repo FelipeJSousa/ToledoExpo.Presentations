27235419cd8c13e47a061f113c67bfa6ffed46b2
Felipe Sousa <felipejosecsousa@gmail.com>
2024-03-27 23:45:15 -0300

    feat(project): init

---
+++ src/ToledoExpo.Presentations.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ToledoExpo.Presentations.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
